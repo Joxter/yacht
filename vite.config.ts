539e1e1a0d88a9cf1ada5d38267824a4edfd7179
Joxter <joxterrr@gmail.com>
2023-07-23 20:31:13 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [
-    /* 
+    /*
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
     // devtools(),
     solidPlugin(),
   ],
+  base: './',
   server: {
     port: 3000,
   },
